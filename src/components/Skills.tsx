3b352b5276a20a90d29befb7b7fedaf0a0df4e7e
Amer Kulovic <amer.kulovic@yahoo.com>
2024-10-31 23:22:48 -0500

    created the skills filter functionality

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,17 +1,31 @@
+import { useEffect, useState } from "react";
+
 const Skills = () => {
+  const frontendSkills: string[] = ["HTML", "CSS", "Javascript", "React", "Vue", "Tailwind CSS", "Typescript"];
+  const backendSkills: string[] = ["Node.js", "Java", "GraphQL", "Mongoose", "SQL"];
+
+  const [selectedSkillset, setSelectedSkillset] = useState<string>("front-end");
+  const displayedSkills = selectedSkillset === "front-end" ? frontendSkills : backendSkills;
+
+  function selectSkills(skillset: string) {
+    setSelectedSkillset(skillset);
+  }
   return (
-    <div className="bg-[#6b21a8]">
+    <div className="bg-[#6b21a8] flex flex-col items-center">
       <h1 className="text-5xl font-bold text-center">Skills</h1>
+      <section className="flex p-5 rounded-lg w-fit">
+        <button className={`p-5 rounded-l-lg ${selectedSkillset === "front-end" ? "bg-[#30124e]" : "bg-[#4d177a]"}`} value="front-end" onClick={(e) => selectSkills((e.target as HTMLButtonElement).value)}>
+          Frontend
+        </button>
+        <button className={`p-5 rounded-r-lg ${selectedSkillset === "back-end" ? "bg-[#30124e]" : "bg-[#4d177a]"}`} value="back-end" onClick={(e) => selectSkills((e.target as HTMLButtonElement).value)}>
+          Backend
+        </button>
+      </section>
       <div className="overflow-x-auto whitespace-nowrap py-4">
         <div className="inline-flex space-x-4">
-          <div className="bg-gray-800 text-white p-4 rounded-lg">HTML</div>
-          <div className="bg-gray-800 text-white p-4 rounded-lg">CSS</div>
-          <div className="bg-gray-800 text-white p-4 rounded-lg">JavaScript</div>
-          <div className="bg-gray-800 text-white p-4 rounded-lg">React</div>
-          <div className="bg-gray-800 text-white p-4 rounded-lg">Vue</div>
-          <div className="bg-gray-800 text-white p-4 rounded-lg">Node.js</div>
-          <div className="bg-gray-800 text-white p-4 rounded-lg">Tailwind CSS</div>
-          <div className="bg-gray-800 text-white p-4 rounded-lg">TypeScript</div>
+          {displayedSkills.map((skill) => (
+            <div className="bg-gray-800 text-white p-4 rounded-lg">{skill}</div>
+          ))}
         </div>
       </div>
     </div>
