bc36de742ff863e04391f85ce9fbdbe21431c1ac
Amer Kulovic <amer.kulovic@yahoo.com>
2024-11-08 10:33:18 -0600

    created the project section

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -11,13 +11,15 @@ const Project = ({ title, image, githubLink, websiteLink }: ProjectProps) => {
   return (
     <div>
       <div className="container inline-block">
-        <img src={image} className="work-picture"></img>
+        <div className="work-picture">
+          <img src={image} alt={title} />
+        </div>
         <h1 className="picture-text">{title}</h1>
         <a className="github-text" href={githubLink} target="_blank">
-          <img src={githubLogo} width="50" height="50"></img>
+          <img src={githubLogo} width="50" height="50" alt="GitHub" />
         </a>
         <a className="link-text" href={websiteLink} target="_blank">
-          <img src={webLogo} width="50" height="50"></img>
+          <img src={webLogo} width="50" height="50" alt="Website" />
         </a>
       </div>
     </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,34 @@
 import Project from "./Project";
+import oceanbuddiesImage from "../assets/images/OceanBuddies.png";
+import novelnestImage from "../assets/images/NovelNest.png";
+import mymoviespaceImage from "../assets/images/MyMovieSpace.png";
+import indianajonesQuizImage from "../assets/images/IndianaJones.png";
+import futurefinderImage from "../assets/images/FutureFinder.png";
+import weatherdashboardImage from "../assets/images/WeatherDashboard.png";
+
+interface ProjectObject {
+  title: string;
+  githubLink: string;
+  websiteLink: string;
+  image: string;
+}
+
+const projects: ProjectObject[] = [
+  { title: "Ocean Buddies", githubLink: "https://github.com/Oceans-4/Ocean-App", websiteLink: "https://ocean-buddies.herokuapp.com/", image: oceanbuddiesImage },
+  { title: "Novel Nest", githubLink: "https://github.com/amerkulovic/NovelNest", websiteLink: "https://amerkulovic.github.io/NovelNest/", image: novelnestImage },
+  { title: "Indiana Jones Quiz", githubLink: "https://github.com/amerkulovic/Indy-Jones-Questionaire", websiteLink: "https://amerkulovic.github.io/Indy-Jones-Questionaire/", image: indianajonesQuizImage },
+  { title: "MyMovieSpace", githubLink: "https://github.com/GroupOneEl/01_group-project", websiteLink: "https://grouponeel.github.io/01_group-project/", image: mymoviespaceImage },
+  { title: "Weather Board", githubLink: "https://github.com/amerkulovic/Weather-Dashboard", websiteLink: "https://amerkulovic.github.io/Weather-Dashboard/", image: weatherdashboardImage },
+  { title: "Future Finder", githubLink: "https://github.com/4-non-non-dorks/future-finder", websiteLink: "https://future-finder.herokuapp.com", image: futurefinderImage },
+];
 const Projects = () => {
   return (
     <div className="bg-gradient-to-b from-[#6b21a8] to-[#1c1c1c] via-[#1c1c1c]">
       <h1 className="text-5xl font-bold text-center">Projects</h1>
       <div className="] py-40 flex flex-wrap justify-center">
-        <Project title="Sample" githubLink="sample" websiteLink="sample" image="sample" />
-        <Project title="Sample" githubLink="sample" websiteLink="sample" image="sample" />
-        <Project title="Sample" githubLink="sample" websiteLink="sample" image="sample" />
-        <Project title="Sample" githubLink="sample" websiteLink="sample" image="sample" />
-        <Project title="Sample" githubLink="sample" websiteLink="sample" image="sample" />
-        <Project title="Sample" githubLink="sample" websiteLink="sample" image="sample" />
+        {projects.map(({ title, githubLink, websiteLink, image }: ProjectObject) => (
+          <Project title={title} githubLink={githubLink} websiteLink={websiteLink} image={image} />
+        ))}
       </div>
     </div>
   );
