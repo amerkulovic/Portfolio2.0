7d3e3ae2bf145efd2e1ac70a989db5acd784f2a6
Amer Kulovic <amer.kulovic@yahoo.com>
2024-11-16 17:53:11 -0600

    added svg to contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import emailjs from "emailjs-com";
+import contactSvg from "../assets/images/contact.svg";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -47,38 +48,41 @@ const Contact = () => {
 
   return (
     <div className="bg-gradient-to-b from-[#1c1c1c] to-black via-[#1c1c1c]">
-      <h1 className="text-5xl font-bold text-center pb-10">Contact</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center w-full max-w-3xl mx-auto px-4">
-        <section className="flex justify-between w-full">
-          <div className="relative w-[48%]">
-            <input className="border border-[#6b21a8] p-5 rounded-lg w-full bg-transparent" type="text" name="firstName" placeholder=" " value={formData.firstName} onChange={handleChange} required />
-            <label className="absolute left-4 top-4 transition-all text-gray-400 pointer-events-none">First Name</label>
-          </div>
-          <div className="relative w-[48%]">
-            <input className="border border-[#6b21a8] p-5 rounded-lg w-full bg-transparent" type="text" name="lastName" placeholder=" " value={formData.lastName} onChange={handleChange} required />
-            <label className="absolute left-4 top-4 transition-all text-gray-400 pointer-events-none">Last Name</label>
-          </div>
-        </section>
-        <section className="flex justify-between w-full mt-4">
-          <div className="relative w-[48%]">
-            <input className="border border-[#6b21a8] p-5 rounded-lg w-full bg-[#1c1c1c]" type="tel" name="phone" placeholder=" " value={formData.phone} onChange={handleChange} required />
-            <label className="absolute left-4 top-4 transition-all text-gray-400 pointer-events-none">Phone Number</label>
-          </div>
-          <div className="relative w-[48%]">
-            <input className="border border-[#6b21a8] p-5 rounded-lg w-full bg-transparent" type="email" name="email" placeholder=" " value={formData.email} onChange={handleChange} required />
-            <label className="absolute left-4 top-4 transition-all text-gray-400 pointer-events-none">Email</label>
-          </div>
-        </section>
-        <section className="w-full mt-4 relative">
-          <textarea style={{ height: "150px" }} className="border border-[#6b21a8] p-5 rounded-lg w-full bg-transparent" name="message" placeholder=" " value={formData.message} onChange={handleChange} required />
-          <label className="absolute left-4 top-4 transition-all text-gray-400 pointer-events-none">Your Message</label>
-        </section>
-        <section className="w-full flex justify-start">
-          <button type="submit" className="mt-6 p-5 bg-[#4d177a] text-white rounded-lg font-bold">
-            Send Message
-          </button>
-        </section>
-      </form>
+      <h1 className="text-5xl font-bold text-center pb-20">Contact</h1>
+      <section className="flex justify-around">
+        <img className="h-[500px] w-[48%] max-md:hidden" src={contactSvg} />
+        <form onSubmit={handleSubmit} className="flex flex-col items-center w-1/2 max-w-3xl mx-auto px-4 max-md:w-full">
+          <section className="flex justify-between w-full">
+            <div className="relative w-[48%]">
+              <input className="border border-[#6b21a8] p-5 rounded-lg w-full bg-transparent" type="text" name="firstName" placeholder=" " value={formData.firstName} onChange={handleChange} required />
+              <label className="absolute left-4 top-4 transition-all text-gray-400 pointer-events-none">First Name</label>
+            </div>
+            <div className="relative w-[48%]">
+              <input className="border border-[#6b21a8] p-5 rounded-lg w-full bg-transparent" type="text" name="lastName" placeholder=" " value={formData.lastName} onChange={handleChange} required />
+              <label className="absolute left-4 top-4 transition-all text-gray-400 pointer-events-none">Last Name</label>
+            </div>
+          </section>
+          <section className="flex justify-between w-full mt-4">
+            <div className="relative w-[48%]">
+              <input className="border border-[#6b21a8] p-5 rounded-lg w-full bg-[#1c1c1c]" type="tel" name="phone" placeholder=" " value={formData.phone} onChange={handleChange} required />
+              <label className="absolute left-4 top-4 transition-all text-gray-400 pointer-events-none">Phone Number</label>
+            </div>
+            <div className="relative w-[48%]">
+              <input className="border border-[#6b21a8] p-5 rounded-lg w-full bg-transparent" type="email" name="email" placeholder=" " value={formData.email} onChange={handleChange} required />
+              <label className="absolute left-4 top-4 transition-all text-gray-400 pointer-events-none">Email</label>
+            </div>
+          </section>
+          <section className="w-full mt-4 relative">
+            <textarea style={{ height: "150px" }} className="border border-[#6b21a8] p-5 rounded-lg w-full bg-transparent" name="message" placeholder=" " value={formData.message} onChange={handleChange} required />
+            <label className="absolute left-4 top-4 transition-all text-gray-400 pointer-events-none">Your Message</label>
+          </section>
+          <section className="w-full flex justify-start">
+            <button type="submit" className="mt-6 p-5 bg-[#4d177a] text-white rounded-lg font-bold">
+              Send Message
+            </button>
+          </section>
+        </form>
+      </section>
     </div>
   );
 };
