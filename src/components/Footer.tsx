2e568fdf2130e5a805c605f9389b088210c4ab78
Amer Kulovic <amer.kulovic@yahoo.com>
2024-11-22 23:47:42 -0600

    adjusted footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,24 +2,24 @@ import githubLogo from "../assets/images/github-white.png";
 
 const Footer = () => {
   return (
-    <div className="bg-black flex justify-between py-12 px-2">
-      <div className="text-5xl font-bold">
+    <div className="bg-black flex justify-between items-center py-12 px-2">
+      <div className="text-5xl font-bold w-[33%] pl-5">
         <span>&lt;</span>Amer <span>/&gt;</span>
       </div>
-      <a href="https://github.com/amerkulovic" target="_blank">
+      <a href="https://github.com/amerkulovic" target="_blank" className="flex justify-center w-[33%]">
         <img src={githubLogo} className="h-12 w-12" />
       </a>
-      <ul className="flex items-center font-bold">
+      <ul className="flex flex-col w-[33%] items-center font-bold">
         {/* <li className="px-4">
           <a href="#about">About</a>
         </li> */}
-        <li className="px-4">
+        <li className="p-4">
           <a href="#skills">Skills</a>
         </li>
-        <li className="px-4">
+        <li className="p-4">
           <a href="#projects">Projects</a>
         </li>
-        <li className="pl-4 mr-10">
+        <li className="pt-4">
           <a href="#contacts">Contact</a>
         </li>
       </ul>
