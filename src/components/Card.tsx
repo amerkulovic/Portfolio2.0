1606c5b5c42add5bcde98b193e54261ce056840b
Amer Kulovic <amer.kulovic@yahoo.com>
2024-11-24 18:58:01 -0600

    adjusted Project and Card components to be more type safe

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export interface CardProps {
   title: string;
   description: string;
@@ -6,7 +8,7 @@ export interface CardProps {
   isFarFromFocus: boolean;
 }
 
-const Card = ({ title, description, isFocused, isNearFocus, isFarFromFocus }: CardProps) => {
+const Card: React.FC<CardProps> = ({ title, description, isFocused, isNearFocus, isFarFromFocus }: CardProps) => {
   const cardClassNames = ["p-4 rounded-lg transition-all duration-300 overflow-hidden", isFocused ? "bg-purple-900 text-white scale-110 opacity-100 w-[250px] h-[250px]" : "", isNearFocus ? "flex items-center justify-center bg-gray-700 text-gray-300 scale-100 opacity-50 w-[200px] h-[200px] text-4xl" : "", isFarFromFocus ? "flex items-center justify-center bg-gray-600 text-gray-400 scale-90 opacity-20 w-[150px] h-[150px] text-2xl" : ""].filter(Boolean).join(" ");
 
   return (
