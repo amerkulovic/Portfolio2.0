125faf30bce896b674ef7249452a09145fc2d38d
Amer Kulovic <amer.kulovic@yahoo.com>
2024-12-03 23:13:40 -0600

    created the about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import Nav from "./components/Nav";
-import About from "./components/About";
+import Home from "./components/Home";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
+import AboutPage from "./components/AboutPage";
 
 function App() {
   return (
     <>
       <Nav />
-      <About />
+      <Home />
+      <AboutPage />
       <Skills />
       <Projects />
       <Contact />
