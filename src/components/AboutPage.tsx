ef1a1f7bdae985635f2e070fe34db034c617fb00
Amer Kulovic <amer.kulovic@yahoo.com>
2024-12-04 13:47:40 -0600

    worked on the about page

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,8 +1,8 @@
 const AboutPage = () => {
   return (
-    <div className="bg-primary">
-      <h1>About</h1>
-      <p>This is the about page</p>
+    <div className="bg-primary flex flex-col items-center pt-20 pb-40">
+      <h1 className="text-5xl font-bold text-center pb-20">About</h1>
+      <p className="text-2xl text-start w-3/4">Hello, my name is Amer. I am a software engineer with a background in business and technology. I am passionate about coding, along with my freelance work, I love creating personal projects to help me grow as a developer. Over the years, I have learned a great deal and can create full-stack projects. My experience as a freelance software engineer has taught me a lot, which has been immensely helpful in this profession. I volunteer for non-profits and help them with their website development!</p>
     </div>
   );
 };
